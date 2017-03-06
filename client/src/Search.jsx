2cda7b7e6664c4a20b901f9193278fb08d5f48dd
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-03-06 11:58:39 +0100

    Separate search input management from main search window

--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -1,88 +1,32 @@
 import React from 'react';
-import {Button, ControlLabel, FormGroup, FormControl, Grid, Row, Col} from 'react-bootstrap';
-import {AdvancedSettings} from './AdvancedSettingsDialog.jsx';
-import {fetchTableLists} from './ComicDBServer';
+import {Grid, Row} from 'react-bootstrap';
+import {SearchInput} from './SearchInput.jsx';
 
 export class Search extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            searchBoxContent : '',
-            searchString: '',
-            searching: false,
-            showSettings: false,
             enabledTables: null,
+            searchResult: null
         };
-        this.retrieveTableNames();
     }
 
-    retrieveTableNames() {
-        const addToState = ls => this.setState({enabledTables: new Map(ls.map(el => [el, false]))});
-        fetchTableLists.then(addToState.bind(this))
-    }
-
-    handleChange(e) {
-        this.setState({searchBoxContent: e.target.value});
-    }
-
-    searchButtonHandler(e) {
-        e.preventDefault();
-        this.setState({
-            searching: true,
-            searchString: this.state.searchBoxContent
-        });
-    }
-
-    toggleSettingsDialog() {
-        this.setState({showSettings: !this.state.showSettings})
+    saveResults(result) {
+        this.setState({searchResult: result})
     }
 
     render() {
         return(
             <div className="search">
             <Grid>
+                <SearchInput onSearchClick={this.saveResults.bind(this)} />
                 <Row>
-                    <form onSubmit={this.searchButtonHandler.bind(this)}>
-                        <FormGroup>
-                            <FormControl
-                                type="text"
-                                value={this.state.searchBoxContent}
-                                placeholder="Type here to search"
-                                onChange={this.handleChange.bind(this)}
-                            />
-                            <FormControl.Feedback/>
-                        </FormGroup>
-                    </form>
-                </Row>
-                <Row>
-                    <Col xs={6} md={4}>
-                        <Button
-                            bsSize="large"
-                            bsStyle="primary"
-                            onClick={this.searchButtonHandler.bind(this)}>
-                                Search
-                        </Button>
-                    </Col>
-                    <Col xs={6} md={4}></Col>
-                    <Col xs={6} md={4}>
-                        <Button
-                            bsSize="large"
-                            bsStyle="success"
-                            onClick={this.toggleSettingsDialog.bind(this)}
-                            >
-                                Advanced Options
-                        </Button>
-                    </Col>
-                </Row>
-               <Row>
-                    {this.state.showSettings && <AdvancedSettings enabledTables={this.state.enabledTables} closeFunction={this.toggleSettingsDialog.bind(this)}/>}
-                    {this.state.searching && <h2> Results </h2>}
-                    {this.state.searching && <p> {"Searching " + this.state.searchString} </p>}
+                   {this.state.searchResult && <h2> Results </h2>}
+                    {this.state.searchResult && <p> {"Searching " + this.state.searchResult} </p>}
                </Row>
             </Grid>
             </div>
         );
     }
-
 }
---
+++ client/src/SearchInput.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {Button, ControlLabel, FormGroup, FormControl, Grid, Row, Col} from 'react-bootstrap';
+import {AdvancedSettings} from './AdvancedSettingsDialog.jsx';
+import {fetchTableLists, search} from './ComicDBServer';
+
+export class SearchInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchBoxContent : '',
+            showSettings: false,
+            enabledTables: null,
+        };
+        this.retrieveTableNames();
+        this.saveResults = props.onSearchClick;
+    }
+
+    onSearchClick(e) {
+        e.preventDefault();
+        search({
+            string: this.state.searchBoxContent,
+            enabledTables: this.state.enabledTables
+        }).then(result => this.saveResults(result));
+    }
+
+    retrieveTableNames() {
+        const addToState = ls => this.setState({enabledTables: new Map(ls.map(el => [el, false]))});
+        fetchTableLists().then(addToState.bind(this))
+    }
+
+    handleChange(e) {
+        this.setState({searchBoxContent: e.target.value});
+    }
+
+    toggleSettingsDialog() {
+        this.setState({showSettings: !this.state.showSettings})
+    }
+
+    render() {
+        return(
+            <Grid>
+                <Row>
+                    <form onSubmit={this.onSearchClick.bind(this)}>
+                        <FormGroup>
+                            <FormControl
+                                type="text"
+                                value={this.state.searchBoxContent}
+                                placeholder="Type here to search"
+                                onChange={this.handleChange.bind(this)}
+                            />
+                            <FormControl.Feedback/>
+                        </FormGroup>
+                    </form>
+                </Row>
+                <Row>
+                    <Col xs={6} md={4}>
+                        <Button
+                            bsSize="large"
+                            bsStyle="primary"
+                            onClick={this.onSearchClick.bind(this)}>
+                                Search
+                        </Button>
+                    </Col>
+                    <Col xs={6} md={4}></Col>
+                    <Col xs={6} md={4}>
+                        <Button
+                            bsSize="large"
+                            bsStyle="success"
+                            onClick={this.toggleSettingsDialog.bind(this)}
+                            >
+                                Advanced Options
+                        </Button>
+                    </Col>
+                </Row>
+               <Row>
+                    {this.state.showSettings && <AdvancedSettings enabledTables={this.state.enabledTables} closeFunction={this.toggleSettingsDialog.bind(this)}/>}
+               </Row>
+               </Grid>
+        );
+    }
+}
