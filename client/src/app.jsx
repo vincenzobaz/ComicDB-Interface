b6b239a5e45de632a40c7f926270484d9476aaf6
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-04-08 11:07:49 +0200

    Add logging middleware for debugging

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {MainWindow} from './MainWindow.js'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import {searchReducer} from './reducers/search.js'
+import {createLogger} from 'redux-logger'
 
-let store = createStore(searchReducer);
+const logger = createLogger({
+	stateTransformer: state => state.toJS()
+});
+let store = createStore(searchReducer, applyMiddleware(logger));
 
 ReactDom.render(
 	<Provider store={store}>
