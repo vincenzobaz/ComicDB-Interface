c910a9329d7cbaf082466f3293446c92303e4818
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-04-29 21:08:42 +0200

    Restyle Tab view

--- client/src/MainWindow.js
+++ client/src/MainWindow.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import {Grid, PageHeader, Row, Tab, Tabs} from 'react-bootstrap';
-import {Search} from './Search.js';
-import {PredefQueries} from './PredefQueries.js';
-import {InsertDelete} from './InsertDelete.js';
+import { Grid, PageHeader, Tab, Tabs } from 'react-bootstrap';
+import { Search } from './Search.js';
+import { PredefQueries } from './PredefQueries.js';
+import { InsertDelete } from './InsertDelete.js';
 
-export function MainWindow(props) {
-    return(
+export const MainWindow = () => {
+    return (
         <Grid>
-            <Row className="navbar">
-                <PageHeader>ComicDB Web Interface</PageHeader>
-                <Tabs id="tab-chooser">
-                    <Tab eventKey={'search'} title="Search"><Search/></Tab>
-                    <Tab eventKey={'prefqueries'} title="Predefined Queries"> <PredefQueries/></Tab>
-                    <Tab eventKey={'insdel'} title="Insert/Delete" ><InsertDelete/></Tab>
-                </Tabs>
-            </Row>
+            <PageHeader>ComicDB Web Interface</PageHeader>
+            <Tabs id="tab-chooser" justified={true}>
+                <Tab eventKey={'insdel'} title="Insert/Delete" ><InsertDelete/></Tab>
+                <Tab eventKey={'search'} title="Search"><Search/></Tab>
+                <Tab eventKey={'prefqueries'} title="Predefined Queries"> <PredefQueries/></Tab>
+            </Tabs>
         </Grid>
     );
-}
+};
