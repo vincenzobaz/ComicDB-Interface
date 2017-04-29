dd682e9fd974c969ad6d383422732cde47f035b9
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-04-29 21:07:32 +0200

    Clean Search window

--- client/src/components/search/SearchInput.js
+++ client/src/components/search/SearchInput.js
@@ -1,4 +1,4 @@
-import React from 'react';
+    import React from 'react';
 import {Button, FormGroup, FormControl, Grid, Row, Col} from 'react-bootstrap';
 import {AdvancedSettings} from './AdvancedSettingsDialog.js';
 import {Server} from './../../ComicDBServer.js'
@@ -15,9 +15,9 @@ class SearchInputV extends React.Component {
         e.preventDefault();
         this.props.onSearchRun(this.state.searchBoxContent);
         this.props.onSearchRun({
-		string: this.state.searchBoxContent,
-		enabledTables: this.props.enabledTables.filter(v => v).toArray()
-	});
+      		string: this.state.searchBoxContent,
+      		enabledTables: this.props.enabledTables.filter(v => v).toArray()
+      	});
     }
 
     handleChange(e) {
@@ -25,45 +25,33 @@ class SearchInputV extends React.Component {
     }
 
     render() {
-        return(
-            <Grid>
-                <Row>
-                    <form onSubmit={this.onSearchClick.bind(this)}>
-                        <FormGroup>
-                            <FormControl
-                                type="text"
-                                value={this.state.searchBoxContent}
-                                placeholder="Type here to search"
-                                onChange={this.handleChange.bind(this)}
-                            />
-                            <FormControl.Feedback/>
-                        </FormGroup>
-                    </form>
-                </Row>
-                <Row>
-                    <Col xs={6} md={4}>
-                        <Button
-                            bsSize="large"
-                            bsStyle="primary"
-                            onClick={this.onSearchClick.bind(this)}>
-                                Search
-                        </Button>
-                    </Col>
-                    <Col xs={6} md={4}></Col>
-                    <Col xs={6} md={4}>
-                        <Button
-                            bsSize="large"
-                            bsStyle="success"
-                            onClick={this.props.onShowSettings}>
-                            Advanced Options
-                        </Button>
-                    </Col>
-                </Row>
-               <Row>
-		       {this.props.showSettings && <AdvancedSettings/>}
-               </Row>
-               </Grid>
-        );
+      return (
+        <form onSubmit={this.onSearchClick.bind(this)}>
+          <FormGroup>
+              <FormControl
+                  type="text"
+                  value={this.state.searchBoxContent}
+                  placeholder="Type here to search"
+                  onChange={this.handleChange.bind(this)}
+              />
+              <FormControl.Feedback/>
+          </FormGroup>
+          <Button
+            bsSize="large"
+            bsStyle="primary"
+            onClick={this.onSearchClick.bind(this)}>
+                Search
+          </Button>
+          <div className="adv-search-settings-button">
+            <Button
+              bsSize="large"
+              bsStyle="success"
+              onClick={this.props.onShowSettings}>
+                Advanced Options
+            </Button>
+          </div>
+	      </form>
+      );
     }
 }
 
@@ -82,4 +70,3 @@ const mapStateToProps = (state) => {
 };
 
 export const SearchInput = connect(mapStateToProps, mapDispatchToProps)(SearchInputV);
-
