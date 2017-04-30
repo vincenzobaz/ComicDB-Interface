d771f32619e649ae48961de88bc9c456c0ec37d4
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-04-30 14:04:17 +0200

    Add title

--- client/src/components/insertdel/InsertPeople.js
+++ client/src/components/insertdel/InsertPeople.js
@@ -4,54 +4,55 @@ import {Button, ControlLabel, FormControl, FormGroup, HelpBlock} from 'react-boo
 import {insert} from './../../actions/insertdel.js';
 
 class InsertPeopleV extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {name : ''};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {name : ''};
+    }
 
-	handleChange(e) {
-		this.setState({name: e.target.value})
-	}
+    handleChange(e) {
+        this.setState({name: e.target.value});
+    }
 
-	isValid() {
-		const name = this.state.name;
-		if (name === "Bob le bricoleur") return 'error';
-		if (name.length <= 3 && name.length > 0) return 'error';
-		if (name.split(' ').length > 2) return 'warning';
-		if (name.length > 3) return 'success' 
-	}
+    isValid() {
+        const name = this.state.name;
+        if (name.length <= 3 && name.length > 0) return 'error';
+        if (name.split(' ').length > 2) return 'warning';
+        if (name.length > 3) return 'success';
+    }
 
-	onSubmit(name) {
-		this.props.submit(this.state.name);
-		this.setState({name: ''});
-	}
+    onSubmit(name) {
+        this.props.submit(this.state.name);
+        this.setState({name: ''});
+    }
 
-	render() {
-		return (
-			<form>
-				<FormGroup validationState={this.isValid()}>
-					<ControlLabel> Name </ControlLabel>
-					<FormControl
-						type="text"
-						value={this.state.name}
-						onChange={this.handleChange.bind(this)}/>
-					<FormControl.Feedback/>
-				</FormGroup>
-				<Button
-					disabled={this.isValid() !== 'success'}
-					onClick={this.onSubmit.bind(this)}>
-					Add to database
-				</Button>
-			</form>
-		);
-	}
+    render() {
+        return (
+            <div className="insertpeople">
+                <h2> Insert a Person </h2>
+                <form>
+                    <FormGroup validationState={this.isValid()}>
+                        <ControlLabel> Name </ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.name}
+                            onChange={this.handleChange.bind(this)}/>
+                        <FormControl.Feedback/>
+                    </FormGroup>
+                    <Button
+                        disabled={this.isValid() !== 'success'}
+                        onClick={this.onSubmit.bind(this)}>
+                        Add to database
+                    </Button>
+                </form>
+            </div>
+        );
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-	return {
-		submit: name => dispatch(insert('People', {name: name}))
-	};
+    return {
+        submit: name => dispatch(insert('People', {name: name}))
+    };
 };
 
-export const InsertPeople = connect(null, mapDispatchToProps)(InsertPeopleV)
-
+export const InsertPeople = connect(null, mapDispatchToProps)(InsertPeopleV);
