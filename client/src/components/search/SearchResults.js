838effd16d7f3e3117e5310eb30bf8a62ef7d699
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-05-02 18:12:14 +0200

    Reformat field name before displaying it

--- client/src/components/search/SearchResults.js
+++ client/src/components/search/SearchResults.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Panel, Table} from 'react-bootstrap';
 
+const prettyTableName = name => name.charAt(0).toUpperCase() + name.slice(1).replace('_', ' ');
+
 const ResultTable = ({fieldNames, data}) => {
-    let i = 0;
     let j = 0;
     let h = 0;
     return (
         <Table responsive>
             <thead>
                 <tr>
-                    {fieldNames.map(f => <th key={i++} >{f}</th>)}
+                    {fieldNames.map(f => <th key={f} >{prettyTableName(f)}</th>)}
                 </tr>
             </thead>
             <tbody>
@@ -23,8 +24,8 @@ export const SearchResults = ({results}) => {
     return (
         <div>
             <h2>Results</h2>
-             {results.map(t =>
-                            <Panel key={i++} header={t.tableName} bsStyle="warning">
+             {results.filter(t => t.data.length != 0).map(t =>
+                            <Panel key={t.tableName} header={t.tableName} bsStyle="warning">
                                 <ResultTable fieldNames={t.fieldNames} data={t.data}/>
                             </Panel>)}
         </div>
